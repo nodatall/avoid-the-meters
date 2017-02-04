3c88b749a5299b00e501f191c231bde1995919e7
Sylvan Swierkosz <sswierkosz@gmail.com>
2017-02-03 23:46:05 -0800

    Add map component

--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -3,56 +3,53 @@ import ReactDOM from 'react-dom'
 import scriptLoader from 'react-async-script-loader'
 
 export default class GoogleMap extends Component {
+
   componentDidMount() {
-          // Connect the initMap() function within this class to the global window context,
-          // so Google Maps can invoke it
-          window.initMap = this.initMap
-          // Asynchronously load the Google Maps script, passing in the callback reference
-          loadJS('https://maps.googleapis.com/maps/api/js?key=AIzaSyBH13poPYygIF6zKN0nol0czm5SFqWNXuY&callback=initMap')
-          
-      }
+    window.initMap = this.initMap
+    this.insertScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyBH13poPYygIF6zKN0nol0czm5SFqWNXuY&callback=initMap')
+  }
 
-    initMap = () => {
-      // load the map
-      const map = new google.maps.Map(ReactDOM.findDOMNode(this), {
-        center: {lat: 40, lng: -100},
-        zoom: 4,
-        styles: mapStyle
-      });
-      console.log('WE ARE HERE')
+  insertScript( src ) {
+    let referenceScript = window.document.getElementsByTagName("script")[0],
+     script = window.document.createElement("script")
 
-      var mapStyle = [{
-        'featureType': 'all',
-        'elementType': 'all',
-        'stylers': [{'visibility': 'off'}]
-      }, {
-        'featureType': 'landscape',
-        'elementType': 'geometry',
-        'stylers': [{'visibility': 'on'}, {'color': '#fcfcfc'}]
-      }, {
-        'featureType': 'water',
-        'elementType': 'labels',
-        'stylers': [{'visibility': 'off'}]
-      }, {
-        'featureType': 'water',
-        'elementType': 'geometry',
-        'stylers': [{'visibility': 'on'}, {'hue': '#5f94ff'}, {'lightness': 60}]
-      }];
+    script.src = src
+    script.async = true
+    referenceScript.parentNode.insertBefore(script, referenceScript)
+  }
+
+  initMap = () => {
+
+    let mapStyle = [{
+      'featureType': 'all',
+      'elementType': 'all',
+      'stylers': [{'visibility': 'on'}]
+    }, {
+      'featureType': 'landscape',
+      'elementType': 'geometry',
+      'stylers': [{'visibility': 'on'}, {'color': '#fcfcfc'}]
+    }, {
+      'featureType': 'water',
+      'elementType': 'labels',
+      'stylers': [{'visibility': 'off'}]
+    }, {
+      'featureType': 'water',
+      'elementType': 'geometry',
+      'stylers': [{'visibility': 'on'}, {'hue': '#5f94ff'}, {'lightness': 60}]
+    }]
+
+    const map = new google.maps.Map(this.refs.map, {
+      center: {lat: 37.801327, lng: -122.274156},
+      zoom: 15,
+      styles: mapStyle
+    })
+
+
   }
 
   render(){
     return(
-      <div>
-        <div ref="map"></div>
-      </div>
+      <div className="googleMap-mapContainer" ref="map"></div>
     )
   }
-}
-
-function loadJS(src) {
-    var ref = window.document.getElementsByTagName("script")[0];
-    var script = window.document.createElement("script");
-    script.src = src;
-    script.async = true;
-    ref.parentNode.insertBefore(script, ref);
 }
